0b7d695821d02f687b8d8cd734a509bfeb2b611d
razalak <razalak916@gmail.com>
2023-10-15 19:14:28 +0530

    small changes

--- routes/users.js
+++ routes/users.js
@@ -2,65 +2,75 @@ var express = require('express');
 var router = express.Router();
 const db = require('../database/connection');
 const bcrypt = require('bcrypt');
-const nodemailer=require('nodemailer');
-const jwt=require('jsonwebtoken');
-const secretKey='key-key';
-const tokengenerator=require('token-generator')({
-  salt: 'secret-key', 
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+const secretKey = 'key-key';
+const tokengenerator = require('token-generator')({
+  salt: 'secret-key',
   timestampMap: 'abcdefghij',
 });
 
+/* Listed All the GET methods here */
 
-          /*Listed All the GET methods here*/
-
-//Routing to intro page
+// Routing to intro page
 router.get('/', function (req, res) {
-  res.render('users/intro');
+  if (req.session.user) {
+    res.redirect('/home');
+  } else {
+    res.render('users/intro');
+  }
+})
+
+// Routing to home page
+router.get('/home', async function (req, res) {
+  const user = await req.session.user;
+  if (!user) {
+    return res.redirect('/login'); // Redirect to login if the user is not logged in
+  }
+  console.log(req.session.id)
+  console.log(user);
+  res.render('users/home', { user }); // Pass user data to the template
 });
 
-
-//Routing to home page
-router.get('/home', function (req, res) {
-  res.render('users/home');
-});
-
-//Routing to login page
+// Routing to login page
 router.get('/login', function (req, res) {
-  // Render the login page and pass the error message as a variable
+  if (req.session.user) {
+    // If the user is already logged in, redirect them to the home page
+    return res.redirect('/home');
+  }
+  // If there is no active session, render the login page and disable caching
+  res.header('Cache-Control', 'no-store');
   res.render('users/login');
 });
 
 
-//routing to password update form
-router.get('/pwupdate',function(req,res){
-  
-  res.render('users/pwupdate'); // Render a form for entering the email
+// Routing to password update form
+router.get('/pwupdate', function (req, res) {
+  res.render('users/pwupdate');
 });
 
-//routing to set new password when clicking confirmation link in mail
+// Routing to set a new password when clicking the confirmation link in the mail
 router.get('/confirmpwupdate/:token', async function (req, res) {
-    const token=req.params.token;
-    const currentTime=Date.now();
+  const token = req.params.token;
+  const currentTime = Date.now();
 
   try {
-
-      // Check if the token exists and is still valid (e.g., within a certain time limit)
+    // Check if the token exists and is still valid (e.g., within a certain time limit)
     const [tokenData] = await db.execute('SELECT * FROM TOKENS WHERE token = ? AND timestamp >= ?', [token, currentTime - 3600000]);
 
     if (tokenData.length === 0) {
       // Token not found or expired
       return res.render('users/pwupdate', { error: 'Invalid or expired token' });
     }
- 
-    res.render('users/confirmpwupdate',{token});
-    
-  } catch (error) {
-    console.error('Error redirecting to set new password',error);
-    res.status(500).json({error:'Server error'});
-    }
-});
-//adding userdetails to database
 
+    res.render('users/confirmpwupdate', { token });
+  } catch (error) {
+    console.error('Error redirecting to set a new password', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// Adding user details to the database
 router.get('/signupsuccess/:token', async function (req, res) {
   const token = req.params.token;
 
@@ -77,7 +87,7 @@ router.get('/signupsuccess/:token', async function (req, res) {
     const email = tokenData[0].email;
 
     // Verify and decode the token using your secret key
-    jwt.verify(token, secretKey,async (err, decoded) => {
+    jwt.verify(token, secretKey, async (err, decoded) => {
       if (err) {
         console.error('Token verification failed:', err);
         return res.status(500).json({ error: 'Server error' });
@@ -88,8 +98,8 @@ router.get('/signupsuccess/:token', async function (req, res) {
 
       // Insert user details into the USERDETAILS table
       const { name, phone, address, Password } = userDetails;
-      console.log(phone)
-      const hashedPassword =await bcrypt.hash(Password,10);
+      console.log(phone);
+      const hashedPassword = await bcrypt.hash(Password, 10);
 
       db.execute('INSERT INTO USERDETAILS (name, phone, address, email,password) VALUES (?, ?, ?, ?,?)',
         [name, phone, address, email, hashedPassword])
@@ -108,16 +118,13 @@ router.get('/signupsuccess/:token', async function (req, res) {
         });
     });
   } catch (error) {
-    console.error('Error in signupsuccess route:', error);
+    console.error('Error in signup success route:', error);
     res.status(500).json({ error: 'Server error' });
   }
 });
 
+/* Listed All the POST methods here */
 
-
-               /*Listed All the POST methods here*/
-
-// Posting signup form
 // Posting signup form
 router.post('/signup', async function (req, res) {
   try {
@@ -143,24 +150,20 @@ router.post('/signup', async function (req, res) {
       return res.render('users/login', { errorMessage }); // Render the login page with the error message
     }
 
-   //const hashedPassword = await bcrypt.hash(Password, 10);
+    const userPayload = {
+      name: Name,
+      phone: Phone,
+      address: Address,
+      Password: Password, // You can omit this if you don't need it in the token
+    };
 
-   const userPayload = {
-    name: Name,
-    phone: Phone,
-    address: Address,
-    Password: Password, // You can omit this if you don't need it in the token
-  };
-
-    //generate a unique token
+    // Generate a unique token
     const token = jwt.sign(userPayload, secretKey, { expiresIn: '1h' });
 
     // Store the token in the database along with the user's email and a timestamp
     await db.execute('INSERT INTO TOKENS(email, token, timestamp) VALUES (?, ?, ?)', [Email, token, Date.now()]);
-
     // Send an email to the user with a link to confirm the email
     confirmationEmail(Email, token);
-
     const success = 'Please check your email for confirmation';
     res.render('users/login', { success }); // Render a success message
 
@@ -179,28 +182,33 @@ router.post('/signin', async function (req, res) {
     // Check if any of the fields are empty
     if (!loginEmail || !loginPassword) {
       const errorMessage = 'Please fill in all fields!!';
-      return res.render('users/login',{errorMessage});
+      return res.render('users/login', { errorMessage });
     }
-   
+
     const [userdata] = await db.execute('SELECT name, email, password FROM USERDETAILS WHERE email=?', [loginEmail]);
-   
-    
+
     if (userdata.length === 0) {
       // User not found, handle this case as needed
       const errorMessage = 'User not found or incorrect password!!';
-      return res.render('users/login',{errorMessage});
+      return res.render('users/login', { errorMessage });
     }
-    const status = await bcrypt.compare(loginPassword, userdata[0].password);
-    if (!status) {
-      
-      const errorMessage = 'Incorrect Password';
-      return res.render('users/login',{errorMessage});
-    }
-
     
+    const user = {
+      name: userdata[0].name,
+      email: userdata[0].email,
+    };
+
+    const status = await bcrypt.compare(loginPassword, userdata[0].password);
+    
+    if (!status) {
+      const errorMessage = 'Incorrect Password';
+      return res.render('users/login', { errorMessage });
+    }
 
     if (status) {
       console.log('User logged in successfully');
+      req.session.loggedIn = true;
+      req.session.user = user;
       res.redirect('/home');
     } else {
       console.log('Sign in failed');
@@ -212,46 +220,44 @@ router.post('/signin', async function (req, res) {
   }
 });
 
-router.post('/pwupdate',async function(req,res){
-      try {
-        const email = req.body.Email;
-        const [userdata]=await db.execute('SELECT * FROM USERDETAILS WHERE email=?',[email]);
-        
-        if (userdata.length === 0) {
-          const errorMessage='User not found';
-          return res.render('users/pwupdate', {errorMessage});
-        }
+router.post('/pwupdate', async function (req, res) {
+  try {
+    const email = req.body.Email;
+    const [userdata] = await db.execute('SELECT * FROM USERDETAILS WHERE email=?', [email]);
+    
+    if (userdata.length === 0) {
+      const errorMessage = 'User not found';
+      return res.render('users/pwupdate', { errorMessage });
+    }
 
-        //generate a unique token
-        const token=tokengenerator.generate();
+    // Generate a unique token
+    const token = tokengenerator.generate();
+    
+    // Store the token in the database along with the user's email and a timestamp
+    await db.execute('INSERT INTO TOKENS(email, token, timestamp) VALUES (?, ?, ?)', [email, token, Date.now]);
+    
+    // Send an email to the user with a link to reset the password
+    sendPasswordResetEmail(email, token);
+    
+    // Render a success message or redirect to a confirmation page
+    const success = 'Confirmation mail sent to your email';
+    res.render('users/pwupdate', { success });
 
-         // Store the token in the database along with the user's email and a timestamp
-        await db.execute('INSERT INTO TOKENS(email, token, timestamp) VALUES (?, ?, ?)', [email, token, Date.now()]);
-
-         // Send an email to the user with a link to reset the password
-        sendPasswordResetEmail(email, token);
-
-         // Render a success message or redirect to a confirmation page
-         const success='confirmation mail sent to your email'
-        res.render('users/pwupdate', { success });
-
-        
-      } catch (error) {
-        console.error('Error reseting password',error);
-        res.status(500).json({error:'server error'});
-      }
+  } catch (error) {
+    console.error('Error resetting password', error);
+    res.status(500).json({ error: 'server error' });
+  }
 });
 
-//posting new password
+// Posting new password
 router.post('/confirmpwupdate/:token', async (req, res) => {
   const token = req.params.token;
   const Password = req.body.Password;
-  
 
   try {
-    if(!Password){
-      const errorMessage='Password should not be null';
-      return res.render('users/confirmpwupdate',{errorMessage,token})
+    if (!Password) {
+      const errorMessage = 'Password should not be null';
+      return res.render('users/confirmpwupdate', { errorMessage, token });
     }
     // Find the email associated with the token
     const [tokenData] = await db.execute('SELECT email FROM TOKENS WHERE token = ?', [token]);
@@ -262,17 +268,15 @@ router.post('/confirmpwupdate/:token', async (req, res) => {
     }
 
     // Hash the new password
-    const hashedPassword = await bcrypt.hash(Password,10);
-   
+    const hashedPassword = await bcrypt.hash(Password, 10);
+    
     // Update the user's password in the database
     await db.execute('UPDATE USERDETAILS SET password = ? WHERE email = ?', [hashedPassword, tokenData[0].email]);
-    
     // Delete the used token from the database
     await db.execute('DELETE FROM TOKENS WHERE token = ?', [token]);
-
     // Render a success message or redirect to a login page
-    const success='Password reset successfully '
-    res.render('users/login',{success});
+    const success = 'Password reset successfully';
+    res.render('users/login', { success });
 
   } catch (error) {
     console.error('Error resetting password:', error);
@@ -280,34 +284,38 @@ router.post('/confirmpwupdate/:token', async (req, res) => {
   }
 });
 
-
-
-
+router.get('/logout', function (req, res) {
+  // Destroy the session to log the user out
+  req.session.destroy(function(err) {
+    if (err) {
+      console.error('Error destroying session:', err);
+    }
+    // Redirect the user to the login page or any other desired page after logout
+    res.redirect('/login');
+  });
+});
 
 module.exports = router;
 
-
-          //Function to confirm email during signup
-async function confirmationEmail(email,token) {
+// Function to confirm email during signup
+async function confirmationEmail(email, token) {
   const transporter = await nodemailer.createTransport({
-    host: "smtp.elasticemail.com",
+    host: 'smtp.elasticemail.com',
     port: 2525,
-  
-  auth: {
-    user: "eventuate80@gmail.com",
-    pass: "AEF77F1296E2077DD72336D86CF749D17BB6"
+    auth: {
+      user: 'eventuate80@gmail.com',
+      pass: 'AEF77F1296E2077DD72336D86CF749D17BB6',
     },
-   
   });
 
   const mailOptions = {
     from: 'eventuate80@gmail.com',
     to: email,
-    subject: 'EVENTUATE Email Verificatoin',
-    text: `Hey user,\nTo confirm your email and complete signup,Please click on the following link:\nhttp://localhost:4000/signupsuccess/${token}`,
+    subject: 'EVENTUATE Email Verification',
+    text: `Hey user,\nTo confirm your email and complete signup, please click on the following link:\nhttp://localhost:4000/signupsuccess/${token}`,
   };
 
-   await transporter.sendMail(mailOptions, (error, info) => {
+  await transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error('Error sending email:', error);
     } else {
@@ -315,21 +323,17 @@ async function confirmationEmail(email,token) {
     }
   });
 }
-         
 
-
-            // Function to send a password reset email
+// Function to send a password reset email
 
 async function sendPasswordResetEmail(email, token) {
   const transporter = await nodemailer.createTransport({
-    host: "smtp.elasticemail.com",
+    host: 'smtp.elasticemail.com',
     port: 2525,
-  
-  auth: {
-    user: "eventuate80@gmail.com",
-    pass: "AEF77F1296E2077DD72336D86CF749D17BB6"
+    auth: {
+      user: 'eventuate80@gmail.com',
+      pass: 'AEF77F1296E2077DD72336D86CF749D17BB6',
     },
-   
   });
 
   const mailOptions = {
@@ -339,7 +343,7 @@ async function sendPasswordResetEmail(email, token) {
     text: `Hey user,\nTo reset your password, click on the following link:\nhttp://localhost:4000/confirmpwupdate/${token}`,
   };
 
-   await transporter.sendMail(mailOptions, (error, info) => {
+  await transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error('Error sending email:', error);
     } else {
@@ -348,3 +352,4 @@ async function sendPasswordResetEmail(email, token) {
   });
 }
 
+module.exports = router;
